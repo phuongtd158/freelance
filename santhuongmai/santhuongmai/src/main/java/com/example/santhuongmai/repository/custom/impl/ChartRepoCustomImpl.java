64a7e3f734d091fc774de646c8ffd5f0cbd0e9d2
Hoadt02 <dotathoa2002@gmail.com>
2024-06-05 22:53:35 +0700

    commit be

--- santhuongmai/santhuongmai/src/main/java/com/example/santhuongmai/repository/custom/impl/ChartRepoCustomImpl.java
+++ santhuongmai/santhuongmai/src/main/java/com/example/santhuongmai/repository/custom/impl/ChartRepoCustomImpl.java
@@ -1,7 +1,119 @@
 package com.example.santhuongmai.repository.custom.impl;
 
+import com.example.santhuongmai.dto.sdi.ChartSdi;
+import com.example.santhuongmai.dto.sdo.ChartSdo;
+import com.example.santhuongmai.repository.custom.ChartRepoCustom;
+import com.example.santhuongmai.util.DataUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.Tuple;
+import java.math.BigDecimal;
+import java.util.List;
+
 @Repository
-public class ChartRepoCustomImpl {
+@RequiredArgsConstructor
+public class ChartRepoCustomImpl implements ChartRepoCustom {
+    private final EntityManager em;
+
+    @Override
+    public List<ChartSdo> searchRevenue(ChartSdi sdi) {
+        String formatDate;
+        String formatField;
+        if (sdi.getCycleType().equalsIgnoreCase("YEAR")) {
+            formatDate = "%Y-01-01";
+            formatField = "%Y";
+        } else if (sdi.getCycleType().equalsIgnoreCase("MONTH")) {
+            formatDate = "%Y-%m-01";
+            formatField = "%m-%Y";
+        } else {
+            formatDate = "%Y-%m-%d";
+            formatField = "%d-%m";
+        }
+
+        String sqlQuery = "WITH RECURSIVE dates(date) AS (SELECT DATE :fromDate\n" +
+                "                               UNION ALL\n" +
+                "                               SELECT date_add(date, interval 1 " + sdi.getCycleType() + ")\n" +
+                "                               FROM dates\n" +
+                "                               WHERE date < DATE :toDate)\n" +
+                "SELECT date_format(date, :formatField) as field, coalesce(sum(o.total_price), 0) as value\n" +
+                "FROM dates d\n" +
+                "         left join orders o on d.date = date_format(o.create_at, :formatDate) and o.status = 9\n" +
+                "group by d.date";
+
+        Query query = em.createNativeQuery(sqlQuery, Tuple.class);
+        query.setParameter("fromDate", sdi.getFromDate());
+        query.setParameter("toDate", sdi.getToDate());
+        query.setParameter("formatDate", formatDate);
+        query.setParameter("formatField", formatField);
+
+        List<Tuple> results = query.getResultList();
+        return DataUtil.convertFromQueryResult(results, ChartSdo.class);
+    }
+
+    @Override
+    public List<ChartSdo> searchProduct(ChartSdi sdi) {
+        String formatDate;
+        String formatField;
+        if (sdi.getCycleType().equalsIgnoreCase("YEAR")) {
+            formatDate = "%Y-01-01";
+            formatField = "%Y";
+        } else if (sdi.getCycleType().equalsIgnoreCase("MONTH")) {
+            formatDate = "%Y-%m-01";
+            formatField = "%m-%Y";
+        } else {
+            formatDate = "%Y-%m-%d";
+            formatField = "%d-%m";
+        }
+
+        String sqlQuery = "WITH RECURSIVE dates(date) AS (SELECT DATE :fromDate\n" +
+                "                               UNION ALL\n" +
+                "                               SELECT date_add(date, interval 1 " + sdi.getCycleType() + ")\n" +
+                "                               FROM dates\n" +
+                "                               WHERE date < DATE :toDate)\n" +
+                "SELECT date_format(date, :formatField) as field, coalesce(sum(od.soluong), 0) as value\n" +
+                "FROM dates d\n" +
+                "         left join orders o on d.date = date_format(o.create_at, :formatDate) and o.status = 9\n" +
+                "         left join order_detail od on o.id = od.order_id\n" +
+                "group by d.date";
+
+        Query query = em.createNativeQuery(sqlQuery, Tuple.class);
+        query.setParameter("fromDate", sdi.getFromDate());
+        query.setParameter("toDate", sdi.getToDate());
+        query.setParameter("formatDate", formatDate);
+        query.setParameter("formatField", formatField);
+
+        List<Tuple> results = query.getResultList();
+        return DataUtil.convertFromQueryResult(results, ChartSdo.class);
+    }
+
+    @Override
+    public Integer getSumQuantityProduct() {
+        return DataUtil.safeToInt(
+                em.createNativeQuery("select sum(quantity) from product").getResultList().get(0)
+        );
+    }
+
+    @Override
+    public BigDecimal getSumRevenue() {
+        return DataUtil.safeToBigDecimal(
+                em.createNativeQuery("select sum(total_price) from orders where status = 9").getResultList().get(0)
+        );
+    }
+
+    @Override
+    public Integer getSumOrderDone() {
+        return DataUtil.safeToInt(
+                em.createNativeQuery("select count(1) from orders where status = 9").getResultList().get(0)
+        );
+    }
+
+    @Override
+    public Integer getSumProductSell() {
+        return DataUtil.safeToInt(
+                em.createNativeQuery("select sum(quantitybuy) from product").getResultList().get(0)
+        );
+    }
 }
