506fab96413a62846e3a78eb449667b0a0db48e7
Hoadt02 <dotathoa2002@gmail.com>
2024-06-05 22:53:45 +0700

    commit fe

---
+++ Frontendv1/src/app/_service/chart.service.ts
@@ -0,0 +1,43 @@
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {SERVER_DOMAIN} from './domain.service';
+
+const CHART_API = SERVER_DOMAIN + "/api/chart/";
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChartService {
+  products: any[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  searchRevenue(data: any): Observable<any> {
+    return this.http.post(CHART_API + 'search-revenue', data, httpOptions);
+  }
+
+  searchProduct(data: any): Observable<any> {
+    return this.http.post(CHART_API + 'search-product', data, httpOptions);
+  }
+
+  getSumQuantityProduct(): Observable<any> {
+    return this.http.get(CHART_API + 'get-sum-quantity-product', httpOptions);
+  }
+
+  getSumRevenue(): Observable<any> {
+    return this.http.get(CHART_API + 'get-sum-revenue', httpOptions);
+  }
+
+  getSumOrderDone(): Observable<any> {
+    return this.http.get(CHART_API + 'get-sum-order-done', httpOptions);
+  }
+
+  getSumProductSell(): Observable<any> {
+    return this.http.get(CHART_API + 'get-sum-product-sell', httpOptions);
+  }
+}
