4dad20f8bba471e69ae80dd60cf8a2ba2b2a51a5
Hoadt02 <dotathoa2002@gmail.com>
2024-07-04 21:56:46 +0700

    commit

--- Frontendv1/src/app/components/common/bubble/bubble.component.ts
+++ Frontendv1/src/app/components/common/bubble/bubble.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-bubble',
@@ -23,6 +23,22 @@ export class BubbleComponent {
         chatButton.classList.remove('shake');
       });
       chatOptions.style.display = 'flex';
+    }
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+    if (scrollPosition > 300) {
+      const element = document.getElementById('chat-container');
+      if (element) {
+        element.style.bottom = '85px';
+      }
+    } else {
+      const element = document.getElementById('chat-container');
+      if (element) {
+        element.style.bottom = '20px';
+      }
     }
   }
 
